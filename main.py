fd5eaed6711244a69915e97d187ddac1f9755d57
Tiana-Fitia <randriatsarafara.fitia@gmail.com>
2026-01-08 18:00:37 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from stream_optimizer import check_stream
-
-app = FastAPI(title="IPTV Stream Optimizer")
-
-@app.get("/check")
-async def check(url: str):
-    return await check_stream(url)
